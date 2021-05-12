20c225d679fd92aa9e7de744702cb2ea2d5532ef
Nick <DAW@DESKTOP-1MD27F1>
2021-05-12 22:05:17 +0200

    otra vez

--- jcolonia.daw2021.prueba2/src/jcolonia/daw2021/prueba2/prueba3.java
+++ jcolonia.daw2021.prueba2/src/jcolonia/daw2021/prueba2/prueba3.java
@@ -3,8 +3,8 @@ package jcolonia.daw2021.prueba2;
 public class prueba3 {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
-
+		System.out.print("hola mundo");
+		System.out.print("hola mundo2");
 	}
 
 }
