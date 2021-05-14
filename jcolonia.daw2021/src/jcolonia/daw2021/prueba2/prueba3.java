971dca64ed3b4845b33045c5605ab77ab843db80
daw1m03 <daw1m03@IC03>
2021-05-14 09:53:21 +0200

    prueba 5

---
+++ jcolonia.daw2021/src/jcolonia/daw2021/prueba2/prueba3.java
@@ -0,0 +1,11 @@
+package jcolonia.daw2021.prueba2;
+
+public class prueba3 {
+
+	public static void main(String[] args) {
+		System.out.print("hola mundo");
+		System.out.print("hola mundo2");
+		System.out.print("hola mundo3");
+	}
+
+}
